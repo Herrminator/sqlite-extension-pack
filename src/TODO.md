f1e4aabf45060c66b1d449a4fb695c0e2ec8df30
Thomas Johler <thomas.johler@msg.group>
2026-01-05 21:52:39 +0100

    Fetch Lua and Lua-extension sources from GitHub

--- src/TODO.md
+++ src/TODO.md
@@ -1,9 +1,9 @@
 # Extensions
-- [ ] sqlite3-lua with `FetchContent` (incl. Lua itself)
+- [x] sqlite3-lua with `FetchContent` (incl. Lua itself)
 - [ ] Bring official extensions up-to-date (3.51.1, like alpine 3.23)
 # Debug / Tests
 - [x] Build `sqlite3` CLI for tests (32/64 bit)
-- [ ] Enable `readline` for sqlite3
+- [ ] Enable `readline` for sqlite3 on Linux
 - [x] CMake tests
 # Build / Deploy
 - Fetch / Clone Git repositories outside build?
--- src/build-bcc.cmd
+++ src/build-bcc.cmd
@@ -3,8 +3,8 @@ call bccenv
 rem make clean extension-functions.dll
 rem vcexpress sqlite_extensions.sln /build %*
 if not exist ..\build\bcc mkdir ..\build\bcc
-if not defined SQLITE_LUA_SOURCE set SQLITE_LUA_SOURCE=sqlite-lua
-if not defined LUA_SOURCE set LUA_SOURCE=%SQLITE_LUA_SOURCE%\lua
+if not defined SQLITE_LUA_SOURCE set "SQLITE_LUA_SOURCE=%~dp0dependencies\sqlite-lua"
+if not defined LUA_SOURCE set "LUA_SOURCE=%~dp0dependencies\lua"
 
 make "-DLUA_SOURCE=%LUA_SOURCE%" "-DSQLITE_LUA_SOURCE=%SQLITE_LUA_SOURCE%" -f makefile.bcc %*
 rem del *.map
