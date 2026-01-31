1db54195279383b9b8df0b121f6cab218bf04207
Herrminator <hhofer69@gmail.com>
2026-02-01 00:50:29 +0100

    Temporary fix for BCC build

--- src/build-bcc.cmd
+++ src/build-bcc.cmd
@@ -3,8 +3,9 @@ call bccenv
 rem make clean extension-functions.dll
 rem vcexpress sqlite_extensions.sln /build %*
 if not exist ..\build\bcc mkdir ..\build\bcc
-if not defined SQLITE_LUA_SOURCE set "SQLITE_LUA_SOURCE=%~dp0dependencies\sqlite-lua"
-if not defined LUA_SOURCE set "LUA_SOURCE=%~dp0dependencies\lua"
+rem HACK ALERT: Now that we let CMake download the sources, we have to assume it built first...
+if not defined SQLITE_LUA_SOURCE set "SQLITE_LUA_SOURCE=%~dp0..\build\x86-release\_deps\sqlite-lua-src"
+if not defined LUA_SOURCE        set "LUA_SOURCE=%~dp0..\build\x86-release\_deps\lua-src"
 
 make "-DLUA_SOURCE=%LUA_SOURCE%" "-DSQLITE_LUA_SOURCE=%SQLITE_LUA_SOURCE%" -f makefile.bcc %*
 rem del *.map
